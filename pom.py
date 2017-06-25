2b9cc058483b661681903c5d44b7fc1b11c6a5fa
Boris Lau <boris@mobify.com>
2017-06-25 13:20:21 -0700

    Added StressQueue detector

--- pom.py
+++ pom.py
@@ -32,20 +32,56 @@ def print_debug(obj):
     })
 
 
+class StreeQueue(object):
+    MAX_QUEUE_SIZE = 3
+    STRESS_THRESHOLD = 50
+
+    def __init__(self):
+        self.queue_list = []
+
+    def add(self, item):
+        self.queue_list.insert(0, item)
+        if len(self.queue_list) > self.MAX_QUEUE_SIZE:
+            self.queue_list.pop()
+
+    @property
+    def avg_stress(self):
+        return (
+            float(sum(self.queue_list)) /
+            max(len(self.queue_list), 1)
+        )
+
+    def check_is_stress(self):
+        return self.avg_stress < self.STRESS_THRESHOLD
+
+
 if __name__=="__main__":
     print("Starting....")
     print(mindwave_obj.start())
+
+    sq = StreeQueue()
+    is_stressed = False
 
     try:
         while True:
             if DEBUG: print_debug(mindwave_obj)
             # print(mindwave_obj.meditation)
-            bar.update(mindwave_obj.meditation)
+            med_value = mindwave_obj.meditation
+
+            sq.add(med_value)
+
+            current_stress = sq.check_is_stress()
+            if (current_stress != is_stressed):
+                is_stressed = current_stress
+                print("Stress: {}".format(current_stress))
+
+            bar.update(med_value)
 
             logfile.write('{}\t{}\n'.format(
                 datetime.now().replace(microsecond=0).isoformat(),
-                mindwave_obj.meditation
+                med_value
             ))
+
             time.sleep(1)
 
     except KeyboardInterrupt:

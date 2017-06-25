390a3c5563dec467f1f9c3357b388e9b4a830bb0
Boris Lau <boris@mobify.com>
2017-06-25 11:15:15 -0700

    Small tidyup on code

--- pom.py
+++ pom.py
@@ -1,31 +1,35 @@
+"""
+Copy and paste from http://pythonhosted.org/NeuroPy/
+"""
+
 import time
 import six
+from pprint import pprint
+
 from NeuroPy import NeuroPy
 
+DEBUG = False
 
-#object1=NeuroPy("COM6")
-
-object1=NeuroPy('/dev/tty.MindWave')
-#If port not given 57600 is automatically assumed
 #object1=NeuroPy("/dev/rfcomm0") for linux
+#object1=NeuroPy("COM6") for windows
+object1=NeuroPy('/dev/tty.MindWave')
 
-def attention_callback(attention_value):
-    "this function will be called everytime NeuroPy has a new value for attention"
-    print "Value of attention is", attention_value
-    #do other stuff (fire a rocket), based on the obtained value of attention_value
-    #do some more stuff
-    return None
+if __name__=="__main__":
+    print("Starting....")
+    print(object1.start())
 
+    while True:
+        if DEBUG:
+            pprint({
+                attr: getattr(object1, attr, 'n/a')
+                for attr in [
+                    'attention',
+                    'meditation',
+                    'poorSignal',
+                    'blinkStrengthrawValue',
+                    'delta',
+                    'theta'
+                ]
+            })
 
-#set call back:
-object1.setCallBack("attention",attention_callback)
-
-#call start method
-print("Starting....")
-print(object1.start())
-
-while True:
-    print("Meditation: {}".format(object1.meditation))
-    time.sleep(1)
-    if(object1.meditation>70): #another way of accessing data provided by headset (1st being call backs)
-        object1.stop()         #if meditation level reaches above 70, stop fetching data from the headset
+        print(object1.meditation)
